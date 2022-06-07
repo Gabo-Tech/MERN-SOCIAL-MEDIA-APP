2c1da0f43eb93bf8923752bfe6a53c8f5308003d
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-06-08 00:52:26 +0200

    nodemailer implemented with bugs

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 
 const express = require("express");
+const path = require('path');
 const app = express();
 const PORT = 1620;
 const { dbConnection } = require("./config/config");
+// const getElementsById = require("getelementsbyid");
+
 
 app.use(express.json());
 
 dbConnection();
+//const btn = document.querySelector('#register');
+const getData = () => {
+    const [name, email, password, age] = getElementsById("name", "email", "password", "age");
 
+
+    var raw = `{\n    \"name\":\"${name.value}\",\n    \"email\":\"${email.value}\",\n    \"password\":\"${password.value}\",\n    \"age\":${age.value}\n}`;
+
+    var requestOptions = {
+    method: 'POST',
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("http://localhost:1620/users/", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+    name.innerHTML = "";
+    email.innerHTML = "";
+    password.innerHTML = "";
+    age.innerHTML = "";
+
+};
+
+//btn.addEventListener(onclick, getData);
+
+
+app.get('/', function(req, res, next){
+    var options = {
+        root: path.join(__dirname)
+    };
+     
+    var fileName = '/public/index.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+});
 app.use('/posts',require('./routes/posts'));
 app.use('/users',require('./routes/users'));
 app.use('/comments',require('./routes/comments'));
